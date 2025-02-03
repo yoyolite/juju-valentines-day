9f73a3201c9fae3aa14b64e5770d18fec558c6c2
yoyolite <orphanisdumb@gmail.com>
2025-02-03 03:26:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function selectOption(option) {
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
-     // Create a new paragraph
-        var yayText = document.createElement('p');
-        yayText.textContent = 'YAYYYY MWAAAAA I LOVE YOU BABYYY! im really grateful to have you in my life and im so happy i could spend this valentine with you and i promise ill always do my best to be a good boyfriend for you and i hope you'll always be happy to be with me and i hope to spend many many other valetines to come with you!!!'; // Set your desired text here
+     // Create a new div
+        var yayText = document.createElement('div');
+        yayText.textContent = 'YAYYYY MWAAAAA I LOVE YOU BABYYY!' // set your desired text here
 
      // Add class for styling
         yayText.className = 'yay-text'; // Apply the CSS class
